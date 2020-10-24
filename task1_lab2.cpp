554fda8064a724bf99bd7aa513b48938d2c4a7f7
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2020-10-24 12:08:07 +0800

    Add files via upload

---
+++ task1_lab2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+long long get_fibbonachi_via_recursion(int number);
+int main()
+{
+    int number;
+    scanf_s("%d", &number);
+    printf("%lld", get_fibbonachi_via_recursion(number));
+
+    return 0;
+}
+
+long long get_fibbonachi_via_recursion(int number)
+{
+    long long buffer[100];
+    buffer[0] = 0;
+    buffer[1] = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        buffer[i % 2] = buffer[0] + buffer[1];
+    }
+
+    return buffer[number % 2];
+}
